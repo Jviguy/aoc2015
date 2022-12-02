43bc66d3d7463f95a292cd5a6752fdc12dd19b7e
Jviguy <62412449+Jviguy@users.noreply.github.com>
2022-12-02 18:38:38 +0000

    day2

--- aoc/day2.go
+++ aoc/day2.go
@@ -1,12 +1,31 @@
 package aoc
 
 import (
+	"fmt"
+	"strconv"
 	"strings"
 )
 
 func DayTwo(input string) {
-	for _, l := range strings.Split(input, "\n") {
-		l, w, h = strings.Split(l, "x")
-
+	t:=0
+	for _, line := range strings.Split(input, "\n") {
+		x := strings.Split(line, "x")
+		s := make([]int,0,len(x))
+		for _, n := range x {
+			w, _ := strconv.Atoi(n)
+			fmt.Println(w)
+			s = append(s, w) 
+		}
+		t += 2*s[0]*s[1] + 2*s[1]*s[2] + 2*s[0]+s[2]
+		fmt.Println(t)
+		smallest := 100
+		for _, side := range []int{s[0]*s[1], s[1]*s[2], s[2]*s[0]} {
+			if side < smallest {
+				smallest = side
+			}
+		}
+		fmt.Println(smallest)
+		t+=smallest
 	}
+	fmt.Printf("Part One Solution: %d\n", t)
 }
